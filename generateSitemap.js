7c506435be033986b679c2a942aad99b66e2c1fe
carlos <karlos.castaneda93@gmail.com>
2023-07-08 01:15:54 -0700

    Feat: deploy sitemap

--- generateSitemap.js
+++ generateSitemap.js
@@ -34,11 +34,6 @@ fetch(`${API_BASE_URL}/posts?limit=150`)
       </urlset>
     `;
 
-		// Check if the directory exists, if not, create it
-		if (!fs.existsSync("sitemap")) {
-			fs.mkdirSync("sitemap");
-		}
-
 		// Write the sitemap to a file
-		fs.writeFileSync("sitemap/sitemap.xml", sitemap);
+		fs.writeFileSync("build/static/sitemap.xml", sitemap);
 	});
